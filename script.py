d8a93408a48b96bfea2a9240af8bca00d42a2b9a
FOURWORDSALLCAPS <48273739+FOURWORDSALLCAPS@users.noreply.github.com>
2023-05-23 22:02:04 +0300

    Fixed bugs in the code

--- script.py
+++ script.py
@@ -1,20 +1,13 @@
-from datacenter.models import Schoolkid
-from datacenter.models import Mark
-from datacenter.models import Chastisement
-from datacenter.models import Lesson
-from datacenter.models import Commendation
-from datacenter.models import Teacher
-from datacenter.models import Subject
+from datacenter.models import *
 from django.core.exceptions import ObjectDoesNotExist
 import random
 
 
 def fix_marks(schoolkid):
-    for child in schoolkid:
-        marks = Mark.objects.filter(schoolkid=child, points__in=[2, 3])
-        for mark in marks:
-            mark.points = 5
-            mark.save()
+    marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
+    for mark in marks:
+        mark.points = 5
+        mark.save()
 
 
 def remove_chastisements(schoolkid):
@@ -29,7 +22,7 @@ def remove_commendation(schoolkid):
         commendation.delete()
 
 
-def create_commendation(schoolkid, lesson):
+def create_commendation(schoolkid, subject):
     commendations = ['Молодец!', 'Ты меня очень обрадовал!', 'Хорошо!',
                      'Хвалю!', 'Великолепно!', 'Замечательно!',
                      'Я тобой горжусь!', 'Сказано здорово – просто и ясно!', 'Потрясающе!',
@@ -37,20 +30,15 @@ def create_commendation(schoolkid, lesson):
                      'Теперь у тебя точно все получится!', ' Очень хороший ответ!', 'Талантливо!'
                      ]
     commendation = random.choice(commendations)
-    subject = Subject.objects.filter(
-        year_of_study__contains="6",
-        title__contains=f"{lesson}"
-    ).first()
     teacher_name = Lesson.objects.filter(
-        year_of_study__contains="6",
-        group_letter__contains="А",
-        subject__title=f'{lesson}'
+        subject=subject,
+        group_letter__contains='А'
     ).first().teacher.full_name
     teacher = Teacher.objects.filter(full_name__contains=f"{teacher_name}").first()
     date_last_lesson = Lesson.objects.exclude(
         year_of_study__contains="6",
         group_letter__contains="А",
-        subject__title__contains=f"{lesson}",
+        subject__title__contains=subject
     ).order_by('-date').first().date
     Commendation.objects.create(
         text=commendation,
@@ -62,10 +50,11 @@ def create_commendation(schoolkid, lesson):
 
 
 def main():
-    child = "Фролов Иван"
-    lesson = 'Музыка'
+    child = 'Фролов Иван'
+    subject = 'Математика'
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=f"{child}")
+        subject = Subject.objects.get(year_of_study__contains="6", title__contains=f"{subject}")
     except ObjectDoesNotExist:
         print("Either the entry or blog doesn't exist.")
 
